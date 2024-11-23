0a7df7bcb58dc8896ec128aa622673d4d77e8a40
nigel0511 <siuting0511@gmail.com>
2024-11-23 16:59:32 +0800

    config

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,17 @@
 import app from "../src/app";
+import connectDB from "../src/config/db";
+
+// Set the environment variable PORT to 3000 if it is not already set.
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Start the application.
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+  connectDB();
+});
 
 export default app;
